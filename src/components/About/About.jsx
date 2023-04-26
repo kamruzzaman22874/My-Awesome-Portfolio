2c3d5cae6e80e03fd363734e2f2fb3c3b159b827
kamruzzaman22874 <joybd.eee@gmail.com>
2023-04-26 22:12:23 +0600

    navbar design

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
 	return (
 		<div
 			name='about'
-			className='w-full md:h-screen bg-[#0a192f] px-5 text-gray-300'>
+			className='w-full md:h-screen bg-[#0a192f] px-5 text-gray-300 pt-20'>
 			<div className='flex flex-col justify-center items-center w-full h-full'>
 				<div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
 					<div className='sm:text-right pb-8 pl-4 text-center'>
